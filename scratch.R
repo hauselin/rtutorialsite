55dfcb983d33a48c97693da1f2eca3ede3f87b6e
Hause Lin <hauselin@gmail.com>
2019-12-01 22:09:09 -0500

    new sigmoid tutorial

--- scratch.R
+++ scratch.R
@@ -1,5 +1,5 @@
 #### create new post/article ####
 library(distill)
-create_post(title = "online classes")
+create_post(title = "Neural networks: Deriving the sigmoid derivative")
 
 #### create new
