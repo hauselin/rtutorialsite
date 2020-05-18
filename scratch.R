6e6f963bdc99e86b626c4e280f8514b65369b516
hauselin <hauselin@gmail.com>
2020-05-17 21:14:16 -0400

    pandas pivot_table post

--- scratch.R
+++ scratch.R
@@ -1,5 +1,5 @@
 #### create new post/article ####
 # library(distill)
-distill::create_post(title = "Reshaping data in Python pandas")
+distill::create_post(title = "Reshape Python pandas dataframe from long to wide with pivot_table")
 
 #### create new
